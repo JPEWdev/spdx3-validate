814ab0111ec3e86508ea75d8981c17dc405140f8
Arthit Suriyawongkul <arthit@gmail.com>
2026-06-04 14:16:38 +0100

    Drop "main" from public API
    Signed-off-by: Arthit Suriyawongkul <arthit@gmail.com>

--- src/spdx3_validate/__init__.py
+++ src/spdx3_validate/__init__.py
@@ -3,11 +3,10 @@
 # SPDX-License-Identifier: MIT
 """spdx3-validate: SPDX 3 validation library and CLI tool."""
 
-from .main import main, spdx3validate
+from .main import spdx3validate
 from .version import VERSION as __version__
 
 __all__ = [
     "__version__",
-    "main",
     "spdx3validate",
 ]
