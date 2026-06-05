cb68812e0705e9b8b74ea4e203b2fcbe753e6bf5
Arthit Suriyawongkul <arthit@gmail.com>
2026-06-05 12:46:25 +0100

    Remove trailing blank line in test_import.py
    Signed-off-by: Arthit Suriyawongkul <arthit@gmail.com>

--- tests/test_import.py
+++ tests/test_import.py
@@ -21,4 +21,3 @@ def test_version_is_exposed():
     """Test that the version is exposed and looks like a version string."""
     assert isinstance(spdx3_validate.__version__, str)
     assert re.match(r"^\d+\.\d+\.\d+", spdx3_validate.__version__)
-
